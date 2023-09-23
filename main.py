f9b9a87e763ddafcaebee79750579165c2afc253
ArtemioPanzini <panferovartemv@gmail.com>
2023-09-24 01:09:16 +0200

    Добавил запись успешных артикулов в файл, readme

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ script_folder = os.path.dirname(os.path.abspath(__file__))
 log_filename = os.path.join(script_folder, "scrapping.log")
 logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8')
 
+def create_dict_from_txt(txt_file_path):
+    result_dict = {}
+    try:
+        with open(txt_file_path, 'r', encoding='utf-8') as txtfile:
+            for line in txtfile:
+                line = line.strip()
+                if line:
+                    key, value = line.split(': ', 1)
+                    result_dict[key] = value
+        return result_dict
+    except FileNotFoundError:
+        logging.error(f'Файл "{txt_file_path}" не найден.')
+        return None
+    except Exception as e:
+        logging.error(f'Произошла ошибка при чтении файла: {str(e)}')
+        return None
+
+
 def download_video(video_dict):
     output_folder = os.path.join(script_folder, "videos")
     output_folder2 = os.path.join(script_folder, "videos", "video_fin")
@@ -41,23 +59,6 @@ def download_video(video_dict):
                             logging.error(f'Не удалось скачать видео "{video_name}". Попытка {attempt + 1}. Повторная попытка через 60 секунд...')
                             time.sleep(60)  # Ждем 60 секунд перед повторной попыткой
 
-def create_dict_from_txt(txt_file_path):
-    result_dict = {}
-    try:
-        with open(txt_file_path, 'r', encoding='utf-8') as txtfile:
-            for line in txtfile:
-                line = line.strip()
-                if line:
-                    key, value = line.split(': ', 1)
-                    result_dict[key] = value
-        return result_dict
-    except FileNotFoundError:
-        logging.error(f'Файл "{txt_file_path}" не найден.')
-        return None
-    except Exception as e:
-        logging.error(f'Произошла ошибка при чтении файла: {str(e)}')
-        return None
-
 def convert_and_delete_video(input_path, video_name):
     try:
         clip = mp.VideoFileClip(input_path)
@@ -69,11 +70,14 @@ def convert_and_delete_video(input_path, video_name):
         # Удаляем исходный видеофайл
         os.remove(input_path)
         logging.info(f'Исходный файл "{video_name}" удален.')
+        with open(success_file_path, 'a', encoding='utf-8') as success_file:
+            success_file.write(f'{video_name}\n')
     except Exception as e:
         logging.error(f'Произошла ошибка при конвертации видео "{video_name}": {str(e)}')
 
+
+success_file_path = os.path.join(script_folder, "successful_articles.txt")
 txt_file_path = os.path.join(script_folder, 'результат_dict.txt')
 result_dict = create_dict_from_txt(txt_file_path)
 
 download_video(result_dict)
-
